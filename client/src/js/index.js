46f8867978e2fc818c9f89557416286b725a14e6
Michael Nguyen <nguyenmichaelm@yahoo.com>
2022-11-04 06:45:50 -0700

    closes #7; configure app to use IndexedDB using idb

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -3,6 +3,7 @@ import "./form";
 import "./submit";
 import { Tooltip, Toast, Popover } from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { initdb } from './database';
 
 // Import CSS files
 import "../css/index.css";
@@ -14,6 +15,7 @@ import Dog from '../images/dog.png';
 
 // Add images on load
 window.addEventListener('load', function () {
+  initdb();
   document.getElementById('logo').src = Logo;
   document.getElementById('bearThumbnail').src = Bear;
   document.getElementById('dogThumbnail').src = Dog;
